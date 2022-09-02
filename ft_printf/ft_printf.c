fb6dda42efc9dcada6b537c4eecf1cccca0e5eb7
key-door <101494707+key-door@users.noreply.github.com>
2022-09-03 02:55:37 +0900

    ft_printf

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,90 +6,146 @@
 /*   By: kyoda <kyoda@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/01 12:20:52 by kyoda             #+#    #+#             */
-/*   Updated: 2022/09/01 16:42:58 by kyoda            ###   ########.fr       */
+/*   Updated: 2022/09/03 02:46:59 by kyoda            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "include/ft_printf.h"
 
-void	ft_sixteen_base(unsigned int n, int flag)
+int	ft_itoa_u_int(long n)
 {
 	char	*str;
 	int		i;
-	char	*p;
+	int		len;
+	int		flag;
 
-	i = 10;
-	if ((str = ft_calloc(11, 1)) == NULL)
-		return ;
-	while (n)
+	flag = 0;
+	if (n < 0)
 	{
-		if (n % 16 < 10)
-			str[--i] = n % 16 + '0x30';
-		else
-			str[--i] = n % 16 + '0x37';
-		n /= 16;
+		flag = 1;
+		n *= -1;
 	}
-	ft_putstr_fd(&str[i], 1);
-	free(str);
-}
-
-void	ft_uitoa(unsigned int n)
-{
-	char	*str;
-	int		i;
-	char	*p;
-
-	i = 10;
-	if ((str = ft_calloc(11, 1)) == NULL)
-		return ;
+	i = 11;
+	if ((str = ft_calloc(12, 1)) == NULL)
+		return (-1);
+	str[i - 1] = 0;
 	while (n)
 	{
 		str[--i] = n % 10 + '0';
 		n /= 10;
 	}
+	if (flag == 1)
+		str[--i] = '-';
 	ft_putstr_fd(&str[i], 1);
+	len = (int)ft_strlen(&str[i]);
 	free(str);
+	return (len);
 }
 
-static void	selection_percent(va_list ap, const char *format, int *re_ans)
+int	ft_itoa_six_base(unsigned int n, int flag)
 {
+	char	*str;
+	int		i;
+	int		len;
+
+	i = 10;
+	if ((str = ft_calloc(11, 1)) == NULL)
+		return (-1);
+	str[i - 1] = 0;
+	while (n)
+	{
+		if (n % 16 < 10)
+			str[--i] = n % 16 + '0';
+		else
+		{
+			str[--i] = n % 16 + '7';
+			if (flag == 0)
+				str[i] = ft_tolower(str[i]);
+		}
+		n /= 16;
+	}
+	ft_putstr_fd(&str[i], 1);
+	len = (int)ft_strlen(&str[i]);
+	free(str);
+	return (len);
+}
+
+int	ft_putptr(va_list ap)
+{
+	char				*str;
+	unsigned long int	n;
+	int					i;
+	int					len;
+
+	n = (unsigned long int)va_arg(ap, void *);
+	i = 10;
+	if ((str = ft_calloc(11, 1)) == NULL)
+		return (-1);
+	str[i - 1] = 0;
+	while (n)
+	{
+		if (n % 16 < 10)
+			str[--i] = n % 16 + '0';
+		else
+			str[--i] = ft_tolower(n % 16 + '7');
+		n /= 16;
+	}
+	str[--i] = 'x';
+	str[--i] = '0';
+	ft_putstr_fd(&str[i], 1);
+	len = (int)ft_strlen(&str[i]);
+	free(str);
+	return (len);
+}
+
+static int	selection_percent(va_list ap, const char *format)
+{
+	char	*p;
+	int		len;
+
+	len = 1;
 	if (*format == 'c')
 		ft_putchar_fd((char)va_arg(ap, int), 1);
 	else if (*format == 's')
-		ft_putstr_fd(va_arg(ap, char *), 1);
+	{
+		p = va_arg(ap, char *);
+		ft_putstr_fd(p, 1);
+		return (ft_strlen(p));
+	}
 	else if (*format == 'p')
-		return ;
-	else if (*format == 'd' || *format == 'i')
-		ft_putnbr_fd(va_arg(ap, int), 1);
+		return (ft_putptr(ap));
+	else if (*format == 'd')
+		return (ft_itoa_u_int(va_arg(ap, int)));
+	else if (*format == 'i')
+		return (ft_itoa_u_int(va_arg(ap, int)));
 	else if (*format == 'u')
-		ft_uitoa(va_arg(ap, unsigned int));
+		return (ft_itoa_u_int(va_arg(ap, unsigned int)));
 	else if (*format == 'x')
-		ft_sixteen_base(va_arg(ap, unsigned int), 0);
+		return (ft_itoa_six_base(va_arg(ap, unsigned int), 0));
 	else if (*format == 'X')
-		ft_sixteen_base(va_arg(ap, unsigned int), 1);
+		return (ft_itoa_six_base(va_arg(ap, unsigned int), 1));
 	else if (*format == '%')
-		return ;
-}
-
-static int	selection_reverse_solidus(void)
-{
-	return (1);
+		ft_putchar_fd('%', 1);
+	return (len);
 }
 
 int	ft_printf(const char *format, ...)
 {
-	int		*re_ans;
+	int		re_ans;
+	int		check;
 	va_list	ap;
-	va_list	argc;
-	char	*day;
 
-	*re_ans = 0;
+	re_ans = 0;
 	va_start(ap, format);
 	while (*format)
 	{
 		if (*format == '%')
 		{
-			selection_percent(ap, ++format, re_ans);
+			check = selection_percent(ap, ++format);
+			if (check == -1)
+				return (0);
+			else
+				re_ans += check;
 			format++;
 		}
 		else
@@ -98,33 +154,19 @@ int	ft_printf(const char *format, ...)
 			{
 				ft_putchar_fd(*format, 1);
 				format++;
+				re_ans += 1;
 			}
 		}
 	}
 	va_end(ap);
-	return (*re_ans);
+	return (re_ans);
 }
 
-int	main(void)
-{
-	ft_printf("c = (%c)\ns = (%s)\nd = (%d)\ni = (%i)\nu = (%u)\nx = (%x)\n",
-				'1',
-				"1234",
-				1234567,
-				-1234,
-				4294967295,
-				256);
-	// ft_printf("s\n%c\nd\n%s\njafs", '1','12345');s
-	// char *p;
-	// p = ft_sixteen_base_min(1000);
-	// printf("%s\n", p);
+// int	main(void)
+// {
+// 	ft_printf(" %s %s %s %s %s ", " - ", "", "4", "", "2 ");
+// 	printf("\n");
+// 	printf(" %s %s %s %s %s ", " - ", "", "4", "", "2 ");
 
-	// unsigned int n;
-	// char a;
-
-	// n = 15 % 16 + 0x00;
-	// printf("%u", n);
-	// a = (char)n;
-	// printf("\n\n\n%c", 0x41-10);
-	return (0);
-}
+// 	return (0);
+// }
