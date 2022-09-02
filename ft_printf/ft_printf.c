26f50569669585b331fd95efc70299341fdb7c91
key-door <101494707+key-door@users.noreply.github.com>
2022-09-03 05:20:38 +0900

    norm-ok

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,129 +6,21 @@
 /*   By: kyoda <kyoda@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/01 12:20:52 by kyoda             #+#    #+#             */
-/*   Updated: 2022/09/03 03:17:04 by kyoda            ###   ########.fr       */
+/*   Updated: 2022/09/03 05:09:40 by kyoda            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "include/ft_printf.h"
 
-int	ft_itoa_u_int(long n)
-{
-	char	*str;
-	int		i;
-	int		len;
-	int		flag;
-
-	flag = 0;
-	if (n == 0)
-	{
-		ft_putchar_fd('0', 1);
-		return (1);
-	}
-	if (n < 0)
-	{
-		flag = 1;
-		n *= -1;
-	}
-	i = 11;
-	if ((str = ft_calloc(12, 1)) == NULL)
-		return (-1);
-	while (n)
-	{
-		str[--i] = n % 10 + '0';
-		n /= 10;
-	}
-	if (flag == 1)
-		str[--i] = '-';
-	ft_putstr_fd(&str[i], 1);
-	len = (int)ft_strlen(&str[i]);
-	free(str);
-	return (len);
-}
-
-int	ft_itoa_six_base(unsigned int n, int flag)
-{
-	char	*str;
-	int		i;
-	int		len;
-
-	i = 10;
-	if (n == 0)
-	{
-		ft_putchar_fd('0', 1);
-		return (1);
-	}
-	if ((str = ft_calloc(11, 1)) == NULL)
-		return (-1);
-	while (n)
-	{
-		if (n % 16 < 10)
-			str[--i] = n % 16 + '0';
-		else
-		{
-			str[--i] = n % 16 + '7';
-			if (flag == 0)
-				str[i] = ft_tolower(str[i]);
-		}
-		n /= 16;
-	}
-	ft_putstr_fd(&str[i], 1);
-	len = (int)ft_strlen(&str[i]);
-	free(str);
-	return (len);
-}
-
-int	ft_putptr(va_list ap)
-{
-	char				*str;
-	unsigned long int	n;
-	int					i;
-	int					len;
-
-	n = (unsigned long int)va_arg(ap, void *);
-	i = 10;
-	if ((str = ft_calloc(11, 1)) == NULL)
-		return (-1);
-	if (n != 0)
-	{
-		while (n)
-		{
-			if (n % 16 < 10)
-				str[--i] = n % 16 + '0';
-			else
-				str[--i] = ft_tolower(n % 16 + '7');
-			n /= 16;
-		}
-	}
-	else
-		str[--i] = '0';
-	str[--i] = 'x';
-	str[--i] = '0';
-	ft_putstr_fd(&str[i], 1);
-	len = (int)ft_strlen(&str[i]);
-	free(str);
-	return (len);
-}
-
 static int	selection_percent(va_list ap, const char *format)
 {
-	char	*p;
-	int		len;
+	int	len;
 
 	len = 1;
 	if (*format == 'c')
 		ft_putchar_fd((char)va_arg(ap, int), 1);
 	else if (*format == 's')
-	{
-		p = va_arg(ap, char *);
-		if (p == NULL)
-		{
-			ft_putstr_fd("(null)", 1);
-			return 6;
-		}
-		ft_putstr_fd(p, 1);
-		return (ft_strlen(p));
-	}
+		return (ft_str_print(ap));
 	else if (*format == 'p')
 		return (ft_putptr(ap));
 	else if (*format == 'd')
@@ -138,9 +30,9 @@ static int	selection_percent(va_list ap, const char *format)
 	else if (*format == 'u')
 		return (ft_itoa_u_int(va_arg(ap, unsigned int)));
 	else if (*format == 'x')
-		return (ft_itoa_six_base(va_arg(ap, unsigned int), 0));
-	else if (*format == 'X')
 		return (ft_itoa_six_base(va_arg(ap, unsigned int), 1));
+	else if (*format == 'X')
+		return (ft_itoa_six_base(va_arg(ap, unsigned int), 2));
 	else if (*format == '%')
 		ft_putchar_fd('%', 1);
 	return (len);
@@ -167,22 +59,10 @@ int	ft_printf(const char *format, ...)
 		}
 		else
 		{
-			while (*format != '%' && *format)
-			{
-				ft_putchar_fd(*format, 1);
-				format++;
-				re_ans += 1;
-			}
+			ft_putchar_fd(*format++, 1);
+			re_ans += 1;
 		}
 	}
 	va_end(ap);
 	return (re_ans);
 }
-
-// int	main(void)
-// {
-// 	ft_printf(" NULL %s NULL ", NULL);
-// 	ft_printf("\n");
-// 	printf(" NULL %s NULL ", NULL);
-// 	return (0);
-// }
--- ft_printf/include/ft_printf.h
+++ ft_printf/include/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: kyoda <kyoda@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/31 18:13:28 by kyoda             #+#    #+#             */
-/*   Updated: 2022/09/01 14:14:33 by kyoda            ###   ########.fr       */
+/*   Updated: 2022/09/03 05:10:50 by kyoda            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,11 @@
 # include "libft.h"
 # include <stdarg.h>
 
-int	ft_printf(const char *format, ...);
+int		ft_printf(const char *format, ...);
+char	*ft_itoarray(unsigned long n, char *str, long base, int flagx);
+int		ft_itoa_u_int(long n);
+int		ft_itoa_six_base(unsigned int n, int flagx);
+int		ft_putptr(va_list ap);
+int		ft_str_print(va_list ap);
 
 #endif
